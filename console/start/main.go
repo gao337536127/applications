4194d7b95f14b40e86a68df9c892b6611797c50c
高文俊 <gwj337536127@163.com>
2024-09-05 16:05:57 +0800

    ini configuration以及status测试

--- console/start/main.go
+++ console/start/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	_ "embed"
+	"fmt"
+	"time"
+
+	"github.com/gao337536127/configuration"
+	"github.com/gao337536127/status"
+)
+
+//go:embed test.ini
+var defaultConfig []byte
+
+func init() {
+	configuration.AppendDefaultConfigurationBytes(defaultConfig)
+}
 
 func main() {
-	fmt.Println("this is test")
+	iniConfig := configuration.IniConfig{}
+	fmt.Println(iniConfig.GetConfigWithEnvironment("fff", "common", "master", "c"))
+
+	status.DefaultStatus.SetStatus("time", func() (string, error) {
+		return fmt.Sprintf("%d", time.Now().Unix()), nil
+	})
+	for i := 0; i < 10; i++ {
+		s, _ := status.DefaultStatus.GetStatus("time")
+		fmt.Printf("s: %v\n", s)
+		time.Sleep(time.Second)
+	}
 }
